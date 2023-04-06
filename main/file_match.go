cfb76ebc935b60767c29bf7c625b1661dc503df7
insppp <freefish_zhou@foxmail.com>
2023-04-06 11:37:26 +0800

    some file match... not ok

--- main/file_match.go
+++ main/file_match.go
@@ -1,8 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	// hello world
-	fmt.Println("Hello, lint!")
+
+	// 获取命令行参数 -p 并设定默认值
+	defaultDir, _ := os.Getwd()
+	pathFlag := flag.String("p", defaultDir, "help massage for -p : the path that the match program works")
+
+	// 解析命令行参数，应该在所有flag声明之后
+	flag.Parse()
+
+	// 绝对路径，输出到log避免被重定向
+	absPath, _ := filepath.Abs(*pathFlag)
+	log.Println("absPath:", absPath)
+
+	entries, _ := os.ReadDir(absPath)
+	for _, entry := range entries {
+		// 获取文件或子目录的信息
+		info, err := entry.Info()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		// 判断是否为目录
+		if info.IsDir() {
+			fmt.Println("Directory:", entry.Name())
+		} else {
+			fmt.Println("File:", entry.Name())
+		}
+	}
 }
